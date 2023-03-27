8da04159ac50d0d8e919208ae7c5191a346b588f
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-03-27 22:20:18 +0500

    Delete votingFactory.sol andd deplooy contract

--- voting_system/scripts/deployVotingSystem.py
+++ voting_system/scripts/deployVotingSystem.py
@@ -1,22 +1,16 @@
-from brownie import VotingFactory, accounts
+from brownie import VotingSystem, accounts
 from dotenv import load_dotenv
 from brownie.network.contract import Contract
 
 load_dotenv()
 
 def main():
-    deployVotingSystem()
+    deployedContract = deployVotingSystem()
     print(f'deployed success! Deployed: {deployedContract}')
 
 def deployVotingSystem():
     account = accounts.load('victor')
-    print('create factory var')
-    factory = VotingFactory[-1]
-
-    votingContract = factory.createNewVoting('testNonFactory', 0, 1, {'from': account, 'priority_fee': '1 wei'})
-
+    votingContract = VotingSystem.deploy({'from': account, 'priority_fee': '1 wei'})
     print(f'votingSystem contract deployed at {votingContract}, type: {type(votingContract)}')
 
     return votingContract
-
-# def counter():
